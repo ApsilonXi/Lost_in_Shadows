170cfc10c01f29ee6f6e2297b12e71592312b380
ApsilonXi <volkov.volkova2000@mail.ru>
2024-12-11 16:25:52 +0300

    0

--- scripts/lost-in-shadows.py
+++ scripts/lost-in-shadows.py
@@ -329,7 +329,7 @@ def battle_screen(hero, enemy):
         pygame.display.update()
 
 def display_text_from_file(file_path):
-    """Чтение текста из файла с возможностью пропустить."""
+    """Чтение текста из файла."""
     with open(file_path, "r", encoding="utf-8") as f:
         lines = f.readlines()
 
